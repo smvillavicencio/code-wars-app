895547565e6a33a3cd48ed65890907652904ea73
nfenciso <nfenciso@up.edu.ph>
2024-01-26 21:40:11 +0800

    feat: implement login, signup, and checkifloggedin
    Still needs to set and remove cookies from client side or the frontend to check if user is logged in and to log out user. Furthermore, additional field added to env file.

--- backend/models/judge.ts
+++ backend/models/judge.ts
@@ -1,8 +1,30 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const JudgeSchema = new mongoose.Schema({
   judge_name: { type: String, required: true },
   password: { type: String, required: true }
 });
+
+JudgeSchema.pre("save", function(next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  return bcrypt.genSalt((saltError: any, salt: any) => {
+      if (saltError) { return next(saltError); }
+
+      return bcrypt.hash(user.password, salt, (hashError: any, hash: any) => {
+      if (hashError) { return next(hashError); }
+
+      user.password = hash;
+      return next();
+      });
+  });
+});
+
+JudgeSchema.methods.comparePassword = function(password: any, callback: any) {
+  bcrypt.compare(password, this.password, callback);
+}
 
 mongoose.model("Judge", JudgeSchema);
--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,15 @@
 // Register models first
 import "./models/judge";
+import "./models/admin";
+import "./models/team";
 
 import express from 'express';
 import bodyParser from 'body-parser';
 import { connectDB, handleDisconnectDB } from './config/db';
 import sampleRoutes from './routes/sampleRoute';
 import loginRoute from './routes/loginRoute';
+import signupRoute from './routes/signupRoute';
+import checkIfLoggedInRoute from './routes/checkIfLoggedInRoute';
 
 
 const app = express();
@@ -20,6 +24,8 @@ app.use(bodyParser.json());
 
 // Routes
 app.use(loginRoute);
+app.use(signupRoute);
+app.use(checkIfLoggedInRoute);
 app.use('/api', sampleRoutes);
 
 // Start the server

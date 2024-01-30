a983dd3091f1aecb11f61bda3a0c6b0a4bbdb8f3
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2024-01-30 20:54:58 +0800

    ui: added placeholder background image

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
-import TopBar from "./components/Topbar";
+import { ThemeProvider } from "@emotion/react";
+import { Box } from "@mui/material";
+// import { theme } from "./theme/index.js";
+import { theme } from "./theme.js";
+
+import bg1 from "./assets/bg1.png";
 
 function App() {
   return (
-    <TopBar />
+    <ThemeProvider theme={theme}>
+      <Box
+        class="bg1"
+        style={{
+          backgroundImage: `url(${bg1})`,
+          backgroundSize: "cover",
+          height: "100vh",
+          color: "#f5f5f5"
+      }}>
+        {/* insert component being tested here */}
+      </Box>
+    </ThemeProvider>
   );
 }
 

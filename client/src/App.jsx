2d38b81077d596a3ffe60c2bb84d85bf97b7e2b4
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2024-02-02 02:26:47 +0800

    chore: reverted App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,20 @@
 import { ThemeProvider } from "@emotion/react";
 import { Box } from "@mui/material";
-
 import { theme } from "./theme.js";
-import bg1 from "./assets/bg1.png";
+import GeneralBackground from "./assets/GeneralBackground.png";
 
-import SubmitModal from "./components/submit-modal/submit-modal.jsx";
-import LogIn from "./pages/login.jsx";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Box
-        class="bg1"
         style={{
-          backgroundImage: `url(${bg1})`,
+          backgroundImage: `url(${GeneralBackground})`,
           backgroundSize: "cover",
           height: "100vh",
           color: "#f5f5f5"
-      }}>
-        {
-          <SubmitModal></SubmitModal>
-        }
+        }}>
+        {/* insert component being tested here */}
       </Box>
     </ThemeProvider>
   );

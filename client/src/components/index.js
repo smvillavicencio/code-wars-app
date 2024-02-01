0b0abd90d00dfb5bb861709af199486eaa756603
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2024-02-01 23:23:19 +0800

    refactor: initial commit for overall leaderboard

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,8 +1,10 @@
+import OverallLeaderboard from "./leaderboard/OverallLeaderboard";
 import SponsorCarousel from "./carousel/SponsorCarousel";
 import ProblemListTable from "./data-table/ProblemListTable";
 import TopBar from "./topbar/Topbar";
 
 export { 
+	OverallLeaderboard,
 	ProblemListTable,
 	SponsorCarousel,
 	TopBar,
---
+++ client/src/components/leaderboard/OverallLeaderboard.jsx
@@ -0,0 +1,8 @@
+const OverallLeaderboard = () => {
+  return (
+    <>
+    </>
+  )
+};
+
+export default OverallLeaderboard;

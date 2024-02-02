a52013156c38d7dcefdb5ebb05132c8f24a1eab9
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2024-02-02 12:00:52 +0800

    ui: created view submissions page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,10 +1,11 @@
 import LogIn from "./login";
 import ViewAllProblemsPage from "./participants/ViewAllProblemsPage";
 import ViewSpecificProblemPage from "./participants/ViewSpecificProblemPage";
-
+import ViewSubmissionsPage from "./judges/ViewSubmissionsPage";
 
 export {
   LogIn,
   ViewAllProblemsPage,
   ViewSpecificProblemPage,
+  ViewSubmissionsPage,
 };

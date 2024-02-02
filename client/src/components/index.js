4843452e370a7de639eed9c976e9d64f0c06734b
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2024-02-02 12:00:31 +0800

    ui: created submissions table component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,11 +3,14 @@ import SponsorCarousel from "./carousel/SponsorCarousel";
 import ProblemListTable from "./tables/ProblemListTable";
 import Timer from "./timer/Timer";
 import TopBar from "./topbar/Topbar";
+import SubmissionsTable from "./tables/SubmissionsTable";
+
 
 export { 
 	OverallLeaderboard,
 	ProblemListTable,
 	SponsorCarousel,
+	SubmissionsTable,
 	Timer,
 	TopBar,
 };

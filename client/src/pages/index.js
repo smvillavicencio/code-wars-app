431cd19b70964dd1a38e02a4733302c7015dd870
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2024-02-03 11:29:30 +0800

    ui: initial power-up logs page for admin

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,12 +1,15 @@
 import LogIn from "./login";
 import GeneralOptionsPage from "./admin/GeneralOptionsPage";
+import PowerUpLogs from "./admin/PowerUpLogs";
 import ViewAllProblemsPage from "./participants/ViewAllProblemsPage";
 import ViewSpecificProblemPage from "./participants/ViewSpecificProblemPage";
 import ViewSubmissionsPage from "./judges/ViewSubmissionsPage";
 
+
 export {
   LogIn,
   GeneralOptionsPage,
+  PowerUpLogs,
   ViewAllProblemsPage,
   ViewSpecificProblemPage,
   ViewSubmissionsPage,

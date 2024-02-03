cc09926329de39a3175c4addab0a37899f69a0e7
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2024-02-03 11:26:44 +0800

    ui: created general options page for admin

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,10 +1,12 @@
 import LogIn from "./login";
+import GeneralOptionsPage from "./admin/GeneralOptionsPage";
 import ViewAllProblemsPage from "./participants/ViewAllProblemsPage";
 import ViewSpecificProblemPage from "./participants/ViewSpecificProblemPage";
 import ViewSubmissionsPage from "./judges/ViewSubmissionsPage";
 
 export {
   LogIn,
+  GeneralOptionsPage,
   ViewAllProblemsPage,
   ViewSpecificProblemPage,
   ViewSubmissionsPage,

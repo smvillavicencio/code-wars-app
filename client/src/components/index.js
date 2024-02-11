2054bfcaa5448fb099b9dc0e13b15af06052f47c
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2024-02-11 17:28:35 +0800

    ui: replaced SubmissionsTable with generic Table component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,7 +4,6 @@ import CustomModal from "./ui/CustomModal";
 import ParticipantsLeaderboard from "./features/leaderboard/ParticipantsLeaderboard";
 import Sidebar from './ui/Sidebar';
 import SponsorCarousel from "./features/carousel/SponsorCarousel";
-import SubmissionsTable from "./tables/SubmissionsTable";
 import Table from "./ui/Table";
 import Timer from "./features/timer/Timer";
 import TopBar from "./ui/Topbar";
@@ -16,7 +15,6 @@ export {
 	ParticipantsLeaderboard,
 	Sidebar,
 	SponsorCarousel,
-	SubmissionsTable,
 	Table,
 	Timer,
 	TopBar,

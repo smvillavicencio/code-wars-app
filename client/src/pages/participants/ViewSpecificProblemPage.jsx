7a65c2a73450fadad57940f6da57109cae981890
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2024-02-12 16:26:19 +0800

    ui: added success window after submitting a file

--- client/src/pages/participants/ViewSpecificProblemPage.jsx
+++ client/src/pages/participants/ViewSpecificProblemPage.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable */ 
-
 import { useState } from 'react';
 
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
@@ -19,69 +18,26 @@ import {
 	Timer,
 	TopBar
 } from 'components/';
+import {
+	columnsLeaderboard,
+	rowsLeaderboard
+} from 'utils/dummyData';
+
 import SubmitModal from './modals/SubmitModal';
 
 
-// dummy data for leaderboard
-const columns = [
-	{
-		field: 'id',
-		headerName: 'ID',
-	},
-	{
-		field: 'rank',
-		headerName: 'Rank',
-		minWidth: 60,
-		maxWidth: 100,
-		headerAlign: 'center',
-		align: 'center',
-		flex: 1,
-	},
-	{
-		field: 'teamName',
-		headerName: 'Team Name',
-		minWidth: 400,
-		// maxWidth: 500,
-		flex: 1,
-	},
-	{
-		field: 'score',
-		headerName: 'Score',
-		minWidth: 150,
-		// maxWidth: 200,
-		flex: 1,
-	},
-	{
-		field: 'totalSpent',
-		headerName: 'Total Spent',
-		minWidth: 100,
-		maxWidth: 200,
-		headerAlign: 'left',
-		align: 'left',
-		flex: 1,
-	},
-];
-
-// dummy data for leaderboard
-const rows = [
-	{ id: 1, rank: 1, teamName: 'Team One', score: 0/200, totalSpent: 1500},
-	{ id: 2, rank: 2, teamName: 'Team Two', score: 0/400, totalSpent: 1300},
-	{ id: 3, rank: 3, teamName: 'Team Three', score: 0/400, totalSpent: 1800},
-	{ id: 4, rank: 4, teamName: 'Team Four', score: 500/500, totalSpent: 1000},
-	{ id: 5, rank: 5, teamName: 'Team Five', score: 300/700, totalSpent: 650},
-	{ id: 6, rank: 6, teamName: 'Team Six', score: 0/1000, totalSpent: 800},
-	{ id: 7, rank: 7, teamName: 'Team Seven', score: 0/2800, totalSpent: 750},
-];
 
 // dummy data
 const problemTitle = 'Hamming distance, interleavings, and isomorphic';
 const problemSubtitle = 'UPLB Computer Science Society';
 const problemDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mauris dolor, euismod nec commodo aliquam, porta vitae ante. Vivamus tincidunt egestas erat nec condimentum. Sed nec ex quis arcu lacinia laoreet. In interdum ipsum orci, ac gravida urna pharetra non. Etiam pretium, ipsum sed volutpat mollis, eros est hendrerit turpis, eget hendrerit libero dui ut eros. Donec sit amet dui sapien. Aliquam nec mi nec mauris placerat gravida. Cras egestas nisl semper semper mollis. Sed dictum augue congue porttitor ultricies. In accumsan, libero at suscipit aliquam, neque lorem eleifend velit, a vulputate lectus lorem in ante.\nMorbi non felis et lorem ultrices porttitor sit amet vitae est. Pellentesque magna urna, posuere a tincidunt a, vehicula sit amet ex. Vestibulum vehicula lectus eget consectetur imperdiet. Aenean interdum ante vel massa ultricies, a aliquet libero tempor. Mauris laoreet ipsum lacus, in iaculis nibh pharetra eget. Nunc eget purus egestas, elementum nulla eget, tincidunt nunc.';
 
-
 /*
  * Purpose: Displays the Specific Problem Page for participants.
- * Params: None
+ * Params:
+ * 		<String> problemTitle - receives the title of the specific problem to be displayed.
+ * 		<String> problemSubtitle - receives the subtitle of the specific problem to be displayed.
+ * 		<String> problemDescription - receives the description of the specific problem to be displayed.
  */
 const ViewSpecificProblemPage = ({
 	// problemTitle,
@@ -100,7 +56,7 @@ const ViewSpecificProblemPage = ({
    */ 
 	const handleButton = () => {
 		setOpen(true);
-	}
+	};
 
 	/*
    * Purpose: Handles on click event of return button and navigates to View All Problems Page
@@ -120,6 +76,7 @@ const ViewSpecificProblemPage = ({
 					<IconButton sx={{ color: '#fff' }} onClick={handleReturn}>
 						<ArrowBackIcon
 							sx={{
+								fontSize: '2rem',
 								cursor: 'pointer',
 								borderRadius: 2,
 								'&:hover': {
@@ -155,7 +112,7 @@ const ViewSpecificProblemPage = ({
 					}}
 				>
 					<Timer />
-					<ParticipantsLeaderboard rows={rows} columns={columns} />
+					<ParticipantsLeaderboard rows={rowsLeaderboard} columns={columnsLeaderboard} />
 					<SponsorCarousel />
 				</Stack>
 
@@ -232,7 +189,7 @@ const ViewSpecificProblemPage = ({
 			
 			{/* Submit Modal Window */}
 			<CustomModal isOpen={open} setOpen={setOpen} windowTitle="Upload your answer">
-				<SubmitModal />
+				<SubmitModal setOpen={setOpen} />
 			</CustomModal>
 
 		</Stack>

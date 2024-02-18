d075537dde3a12660beb4f4c4d9058d189c2bd07
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2024-02-18 09:21:46 +0800

    chore: updated import paths for moved files

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,17 +1,21 @@
 /* eslint-disable */ 
+import Buff from './widgets/power-ups/Buff';
+import Debuff from './widgets/power-ups/Debuff';
 import DropdownSelect from "./ui/DropdownSelect";
 import CustomModal from "./ui/CustomModal";
 import LogTable from "./tables/LogTable";
-import ParticipantsLeaderboard from "./features/leaderboard/ParticipantsLeaderboard";
+import ParticipantsLeaderboard from "./widgets/leaderboard/ParticipantsLeaderboard";
 import Sidebar from './ui/Sidebar';
-import SponsorCarousel from "./features/carousel/SponsorCarousel";
+import SponsorCarousel from "./widgets/carousel/SponsorCarousel";
 import SubmissionsTable from "./tables/SubmissionsTable";
 import Table from "./ui/Table";
-import Timer from "./features/timer/Timer";
+import Timer from "./widgets/timer/Timer";
 import TopBar from "./ui/Topbar";
 
 
 export { 
+	Buff,
+	Debuff,
 	DropdownSelect,
 	CustomModal,
 	LogTable,

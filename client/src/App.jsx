3d1bf3a1b5dcf23d8823f63648a23cc2751b9745
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2024-02-26 01:30:22 +0800

    chore: deleted UserDetailsProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,19 +18,43 @@ import {
 } from 'pages/';
 import { theme } from 'theme.js';
 
-import { userDetailsContext } from 'utils/UserDetailsProvider';
+import { OverlayProvider, overlayContext } from 'utils/OverlayProvider';
 import { baseURL } from 'utils/constants';
 import { postFetch } from 'utils/apiRequest';
-import { UserDetailsProvider } from 'utils/UserDetailsProvider.js';
 import Cookies from "universal-cookie";
 import { socketClient } from 'socket/socket';
 
+
+
 var immortalHTML = '<div class="MuiBox-root css-1ato3wx"><div class="MuiBox-root css-1xpmd5v"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium fOverlay css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="HourglassFullTwoToneIcon" style="font-size: 10rem; align-self: center;"><path d="m8 7.5 4 4 4-4V4H8zm0 9V20h8v-3.5l-4-4z" opacity=".3"></path><path d="M18 2H6v6h.01L6 8.01 10 12l-4 4 .01.01H6V22h12v-5.99h-.01L18 16l-4-4 4-3.99-.01-.01H18zm-2 14.5V20H8v-3.5l4-4zm0-9-4 4-4-4V4h8z"></path></svg><br><h4 class="MuiTypography-root MuiTypography-h4 fOverlay css-1cvibvw-MuiTypography-root">Your screen has been frozen. <br>Please Wait.</h4></div></div>';
 
-function App() {
 
-	const [freezeOverlay, setFreezeOverlay] = useState(false);
-	const overlayLoad = useRef(false);
+/**
+ * This will set the common background for all pages (except login page)
+ */
+function Layout() {
+	return (
+		<Box
+			style={{
+				backgroundImage: `url(${GeneralBackground})`,
+				backgroundSize: 'cover',
+				height: '100vh',
+				overflow: 'hidden',
+			}}
+			id="commonBox"
+		>
+			{/* Children will be displayed through outlet */}
+			<Outlet />
+		</Box>
+	);
+}
+
+
+function App() {
+	const [freezeOverlay, setFreezeOverlay] = useState(overlayContext);
+	// const [loadingOverlay, setLoadingOverlay] = useState(overlayContext);
+
+	const overlayFreezeLoad = useRef(false);
 
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -55,87 +79,64 @@ function App() {
 		//}, 1000);
 	}
 
-	/**
-	 * This will set the common background for all pages (except login page)
-	 */
-	function Layout() {
-		return (
-			<Box
-				style={{
-					backgroundImage: `url(${GeneralBackground})`,
-					backgroundSize: 'cover',
-					height: '100vh',
-					overflow: 'hidden',
-				}}
-				id="commonBox"
-			>
-				{/* Children will be displayed through outlet */}
-				{freezeOverlay ? <div className='fOverlayScreen' style={{zIndex: "10000"}}><FreezeOverlay /></div> : null}
-				<Outlet />
-			</Box>
-		);
-	}
-
-	// state for context API
-	//const [userDetails, setUserDetails] = useContext(userDetailsContext ?? null);
 
 	useEffect(() => {
 		const eventSource = new EventSource(`${baseURL}/admincommand`);
 		eventSource.onmessage = (e) => {
-			var adminMessage = JSON.parse(e.data);
-			if (adminMessage.command == "login") {
-				console.log(adminMessage);
-			}
+			// getting admin message
+			let adminMessage = JSON.parse(e.data);
 
+			// for participants
 			if (JSON.parse(localStorage?.getItem("user"))?.usertype == "participant") {
-			
+				
+				if (adminMessage.command == "freeze") {
+					setFreezeOverlay(true);
 
-			if (adminMessage.command == "freeze") {
-				setFreezeOverlay(true);
+					// if hindi pa naka-display yung freeze overlay, set to true
+					if (document.querySelectorAll(".fOverlayScreen")[0] != null && overlayFreezeLoad.current == false) {
+						overlayFreezeLoad.current = true;
+					}
 
+					// checks if present yung elements na may fOverlay
+					let checker = document.getElementsByClassName("fOverlay").length;
 
-				if (document.querySelectorAll(".fOverlayScreen")[0] != null && overlayLoad.current == false) {
-					overlayLoad.current = true;
+					setTimeout(() => {
+						// if hindi pa naka-display yung component pero naka-true na yung overlay, i-display na
+						if (checker < 2 && overlayFreezeLoad.current == true) {
+
+							try {
+								document.getElementsByClassName("fOverlayScreen")[0].remove();
+							} catch (error) {
+								
+							}
+							
+							const immortalDiv = document.createElement('div');
+							immortalDiv.className = "fOverlayScreen";
+							immortalDiv.style.zIndex = "10000";
+							immortalDiv.innerHTML = immortalHTML;
+
+							let commonBox = document.getElementById("commonBox");
+							commonBox.insertBefore(immortalDiv, commonBox.firstChild);
+						}
+					}, 1000);
+				} 
+				else if (adminMessage.command == "logout") {
+					console.log("Should log out");
+
+					setFreezeOverlay(false);
+					localStorage.removeItem("user");
+									//setUserDetails(null);
+									window.location.replace(window.location.origin);
+
+									// Delete cookie with authToken
+									const cookies = new Cookies();
+									cookies.remove("authToken");
+				} 
+				else if (adminMessage.command == "normal") {
+					setFreezeOverlay(false);	
 				}
 
-				let checker = document.getElementsByClassName("fOverlay").length;
-
-				setTimeout(()=>{
-					if (checker < 2 && overlayLoad.current == true) {
-
-						try {
-							document.getElementsByClassName("fOverlayScreen")[0].remove();
-						} catch (error) {
-							
-						}
-						
-						const immortalDiv = document.createElement('div');
-						immortalDiv.className = "fOverlayScreen";
-						immortalDiv.style.zIndex = "10000";
-						immortalDiv.innerHTML = immortalHTML;
-
-						let commonBox = document.getElementById("commonBox");
-						commonBox.insertBefore(immortalDiv, commonBox.firstChild);
-					}
-				}, 1000);
-			} 
-			else if (adminMessage.command == "logout") {
-				console.log("Should log out");
-
-				setFreezeOverlay(false);
-				localStorage.removeItem("user");
-                //setUserDetails(null);
-                window.location.replace(window.location.origin);
-
-                // Delete cookie with authToken
-                const cookies = new Cookies();
-                cookies.remove("authToken");
-			} 
-			else if (adminMessage.command == "normal") {
-				setFreezeOverlay(false);	
-			}
-
-			
+				
 
 			}
 			if (adminMessage.command == "freeze") {
@@ -158,7 +159,7 @@ function App() {
 
 	return (
 		<ThemeProvider theme={theme}>
-			<UserDetailsProvider>
+			<OverlayProvider>
 				<Router>
 					<Routes>
 						{/* Login page */}
@@ -167,16 +168,34 @@ function App() {
 						{/* Pages with same backgrounds */}
 						<Route path="/" element={<Layout />}>
 							<Route path="participant/view-all-problems" 
-								element={<ViewAllProblemsPage 
-									isLoggedIn={isLoggedIn} 
-									setIsLoggedIn={setIsLoggedIn} 
-									checkIfLoggedIn={checkIfLoggedIn}
-									currRound={currRound}
-									setCurrRound={setCurrRound}
-									//seconds={sec}
-									 />} />
-							<Route path="participant/view-specific-problem" element={<ViewSpecificProblemPage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} checkIfLoggedIn={checkIfLoggedIn} />} />
-							<Route path="judge/submissions" element={<ViewSubmissionsPage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} checkIfLoggedIn={checkIfLoggedIn} />} />
+								element={
+									<ViewAllProblemsPage 
+										isLoggedIn={isLoggedIn} 
+										setIsLoggedIn={setIsLoggedIn} 
+										checkIfLoggedIn={checkIfLoggedIn}
+										currRound={currRound}
+										setCurrRound={setCurrRound}
+										//seconds={sec}
+									/>}
+							/>
+							<Route path="participant/view-specific-problem"
+								element={
+									<ViewSpecificProblemPage
+										isLoggedIn={isLoggedIn}
+										setIsLoggedIn={setIsLoggedIn}
+										checkIfLoggedIn={checkIfLoggedIn}
+									/>
+								}
+							/>
+							<Route path="judge/submissions"
+								element={
+									<ViewSubmissionsPage
+										isLoggedIn={isLoggedIn}
+										setIsLoggedIn={setIsLoggedIn}
+										checkIfLoggedIn={checkIfLoggedIn}
+									/>
+								}
+							/>
 							<Route path="admin/general" 
 								element={<GeneralOptionsPage 
 									isLoggedIn={isLoggedIn} 
@@ -189,13 +208,29 @@ function App() {
 									checked={checked}
 									setChecked={setChecked}
 									/>} />
-							<Route path="admin/logs" element={<PowerUpLogs isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} checkIfLoggedIn={checkIfLoggedIn} />} />
-							<Route path="admin/podium" element={<TopTeamsPage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} checkIfLoggedIn={checkIfLoggedIn} />} />
+							<Route path="admin/logs"
+								element={
+									<PowerUpLogs
+										isLoggedIn={isLoggedIn}
+										setIsLoggedIn={setIsLoggedIn}
+										checkIfLoggedIn={checkIfLoggedIn}
+									/>
+								}
+							/>
+							<Route path="admin/podium"
+								element={
+									<TopTeamsPage
+										isLoggedIn={isLoggedIn}
+										setIsLoggedIn={setIsLoggedIn}
+										checkIfLoggedIn={checkIfLoggedIn}
+									/>
+								}
+							/>
 						</Route>
 					</Routes>
 					<ToastContainerConfig />
 				</Router>
-			</UserDetailsProvider>
+			</OverlayProvider>
 		</ThemeProvider>
 	);
 }

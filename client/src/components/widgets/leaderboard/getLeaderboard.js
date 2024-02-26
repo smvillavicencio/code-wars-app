e2bc022b6f48f4eb498e29571e564a177bce4d99
Lara Patricia Pineda <lbpineda1@up.edu.ph>
2024-02-26 14:38:49 +0800

    refactor: modified leaderboard code for ranking

--- client/src/components/widgets/leaderboard/getLeaderboard.js
+++ client/src/components/widgets/leaderboard/getLeaderboard.js
@@ -7,27 +7,29 @@ export default async function getLeaderboard() {
   let data = await getFetch(`${baseURL}/viewLeaderboard`,);
   let currLeaderboard = [];
 
-  data.leaderboard.map((entry) => {
-    let newEntry = {};
-
-    newEntry.rank = "";
-    newEntry.id = entry._id;
-    newEntry.team_name = entry.team_name;
-    newEntry.score = entry.score;
-    newEntry.total_points_used = entry.total_points_used;
-
-    currLeaderboard.push(newEntry);
-  })
-
-  // sort leaderboard descending order
-  currLeaderboard.sort((a, b) => {
-    return b.score - a.score;
-  });
-
-  // add ranks to field values
-  currLeaderboard.map((entry, index) => {
-    entry.rank = index + 1;
-  })
+  if (data.leaderboard.length > 0) {
+    data.leaderboard.map((entry) => {
+      let newEntry = {};
+  
+      newEntry.id = entry._id;
+      // newEntry.rank = entry.rank;
+      newEntry.team_name = entry.team_name;
+      newEntry.score = entry.score;
+      newEntry.total_points_used = entry.total_points_used;
+  
+      currLeaderboard.push(newEntry);
+    })
+  
+    // sort leaderboard descending order
+    currLeaderboard.sort((a, b) => {
+      return b.score - a.score;
+    });
+  
+    // add ranks to field values
+    currLeaderboard.map((entry, index) => {
+      entry.rank = index + 1;
+    })
+  }
 
   return currLeaderboard;
 }
